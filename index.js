09d24f4b00ed1afc454f8bef071144d2608129ad
Yashi Gupta <98335217+yashigupta4623@users.noreply.github.com>
2023-05-01 23:16:41 +0530

    adding the HTML files

---
+++ index.js
@@ -0,0 +1,5 @@
+var number = =0;i<document.querySelectorAll("document.querySelectoryAll"(.drum").length;
+
+document.querySelector("button").addEventListener("click",function(){
+    alert("I got Clicked!");
+});
